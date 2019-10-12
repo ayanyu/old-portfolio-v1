e0a61af6784adbbe29500b0ba0c555d1276208f6
Ayan Yusuf <47934712+ayanyu@users.noreply.github.com>
2019-10-12 13:03:24 -0400

    Update form2email.php

--- form2email.php
+++ form2email.php
@@ -1,37 +1,10 @@
 
 
 <?php
-
-  $name = $_POST['name'];
-
-  $visitor_email = $_POST['email'];
-
-  $subject = $_POST('subject');
-
-  $message = $_POST['message'];
-
-  ?>
-
-<?PHP
- $email_from = 'yourname@yourwebsite.com';
-
-    $email_subject = "New Form submission";
-
-    $email_body = "You have received a new message from the user $name.\n".
-  "Here is the message:\n $message".
-
+if($_POST["message"]) {
+    mail("ayanhaibeh@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+}
 ?>
-
-<?php
-$to = "ayanhaibeh@gmail.com";
-
-  $headers = "From: $email_from \r\n";
-
-  $headers .= "Reply-To: $visitor_email \r\n";
-
-  mail($to,$email_subject,$email_body,$headers);
-
-  ?>
 
 
 
