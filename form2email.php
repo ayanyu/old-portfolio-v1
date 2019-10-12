d9a39b3194f279a978f316fcb4eff42b051f8c1b
Ayan Yusuf <47934712+ayanyu@users.noreply.github.com>
2019-10-12 15:20:29 -0400

    Update form2email.php

--- form2email.php
+++ form2email.php
@@ -1,8 +1,19 @@
 
 
 <?php
-if($_POST["message"]) {
-    mail("ayanhaibeh@gmail.com", "Form to email message", $_POST["message"], "From: an@email.address");
+
+if (isset($_POST['submit'])) {
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$mailFrom = $_POST['email'];
+$message = $_POST['message'];
+
+$mailTo = "ayanhaibeh@gmail.com";
+$headers = "From: ".$mailFrom;
+$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+mail($mailTo, $subject, $txt, $headers);
+header("Location: index.php?mailsend");
 }
 ?>
 
